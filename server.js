fc15c6e0e938d2a45f2b65f74c94e99588a8551e
pritam <pritam.saha@webskitters.com>
2021-07-07 18:47:18 +0530

    model

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express=require('express');
 const dotenv=require('dotenv');
-const logger=require('./middleware/logger')
-const morgan=require('morgan')
-//Route files
-const bootcamps=require('./routes/bootcamps')
-
+const morgan=require('morgan');
+const colors=require('colors');
+const connectDB=require('./config/db');
 
 //Load env files
 dotenv.config({path:'./config/config.env'});
+
+//Connect to Database
+connectDB();
+//Route files
+const bootcamps=require('./routes/bootcamps')
 const app=express();
 // Dev logging Middleware
 if(process.env.NODE_ENV ==='development'){
@@ -17,5 +20,11 @@ if(process.env.NODE_ENV ==='development'){
 app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT,console.log(`Server running into ${process.env.NODE_ENV}
- mode on port ${PORT}`));
+ const server=app.listen(PORT,console.log(`Server running into ${process.env.NODE_ENV}
+ mode on port ${PORT}`.yellow.bold));
+ //Handle unhandled rejection
+ process.on('unhandleRejection',(err,promise)=>{
+    console.log(`Error:${err.message}`.red) 
+    //close server & exit process
+    server.close(()=>process.exit(1));
+ })
