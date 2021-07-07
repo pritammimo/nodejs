1fa0e532c0523c1ae4d3c0871afc02db97a53cab
pritam <pritam.saha@webskitters.com>
2021-07-07 11:49:13 +0530

    gitignore

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const dotenv=require('dotenv');
 dotenv.config({path:'./config/config.env'});
 const app=express();
 const PORT=process.env.PORT || 5000;
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV}
+app.listen(PORT,console.log(`Server running into ${process.env.NODE_ENV}
  mode on port ${PORT}`));
