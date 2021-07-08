2d0a61e3306075d7b17f7d9c9c8b3365bf308ca8
pritam <pritam.saha@webskitters.com>
2021-07-08 22:43:42 +0530

    Mongoose bad object

--- middleware/error.js
+++ middleware/error.js
@@ -4,11 +4,16 @@ const errorHandler=(err,req,res,next)=>{
     let error={...err}
     error.message=err.message;
 //Log to console
-console.log(err.stack.red);
+console.log(err);
 //Mongoose bad ObjectId
 if(err.name==="CastError"){
     const message = `Resource not found with id of ${err.value}`;
     error=new ErrorResponse(message,404);
+}
+//Mongoose Duplicate Key
+if(err.code === 11000){
+    const message='Duplicate field value entered';
+    error=new ErrorResponse(message,400)
 }
 res.status(error.statusCode||500).json({
     success:false,
