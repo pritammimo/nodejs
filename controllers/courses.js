30b0eacc432ea96de7f496f8e95458e1cec318a2
pritam <pritam.saha@webskitters.com>
2021-07-11 22:44:29 +0530

    update course

--- controllers/courses.js
+++ controllers/courses.js
@@ -66,3 +66,29 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     data: course,
   });
 });
+//Update   Course
+//route PUT/api/v1/courses/:id
+//@access   Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+let course=await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(
+        `No course with the id of ${req.params.id}`,
+        404
+      )
+    );
+  }
+  course=await Course.findByIdAndUpdate(
+      req.params.id,
+      req.body,{
+         new:true,
+         runValidators:true 
+      }
+  );
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});//4 min 31 sec
--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const {
- getCourses,getCourse,addCourse
+ getCourses,getCourse,addCourse,updateCourse
 } = require("../controllers/courses");
 const router = express.Router({mergeParams:true});
 
 router.route("/").get(getCourses).post(addCourse);
-router.route('/:id')
-.get(getCourse)
+router.route('/:id').put(updateCourse)
+.get(getCourse);
 
 
 module.exports = router;
