6f403c395a7c609b9225682a8f523ff3b776bd37
pritam <pritam.saha@webskitters.com>
2021-07-13 22:35:37 +0530

    check

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -153,6 +153,6 @@ exports.getBootcampsInRadius=asyncHandler(async(req,res,next)=>{
      res.status(200).json({
        success:true,
        count:bootcamps.length,
-       data:bootcamps
+       data:bootcamps //for check
      })
 })
