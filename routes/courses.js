72c4d751061cb42be6f9f9077e1439f61fbd73ca
pritam <pritam.saha@webskitters.com>
2021-07-17 21:39:49 +0530

    role authorization

--- routes/courses.js
+++ routes/courses.js
@@ -2,17 +2,26 @@ const express = require("express");
 const {
  getCourses,getCourse,addCourse,updateCourse,deleteCourse
 } = require("../controllers/courses");
+const { protect,authorize } = require("../middleware/auth");
 const Course=require('../models/Course');
 const advancedResults=require('../middleware/advancedResults')
 const router = express.Router({mergeParams:true});
 
-router.route("/").get(
-    advancedResults(Course,{
-        path:'bootcamp',
-        select:'name description'
-    }),getCourses).post(addCourse);
-router.route('/:id').put(updateCourse).delete(deleteCourse)
-.get(getCourse);
+router
+  .route("/")
+  .get(
+    advancedResults(Course, {
+      path: "bootcamp",
+      select: "name description",
+    }),
+    getCourses
+  )
+  .post(protect, authorize("publisher", "admin"), addCourse);
+router
+  .route("/:id")
+  .put(protect, authorize("publisher", "admin"), updateCourse)
+  .delete(protect, authorize("publisher", "admin"), deleteCourse)
+  .get(getCourse);
 
 
 module.exports = router;
